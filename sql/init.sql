ca13662433b3ad6f53d8a3f90dd0e6f359691154
Jimmy Goffaux <jimmy@gofaux.fr>
2015-05-03 19:23:16 +0000

    init SQL

--- sql/init.sql
+++ sql/init.sql
@@ -1,8 +1,22 @@
+
+
 DROP TABLE IF EXISTS `club`;
+DROP TABLE IF EXISTS `contact`;
+
 
 CREATE TABLE `club` (
-  `ID` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `Name` varchar(100)
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(100)
 );
 
 
+CREATE TABLE `contact` (
+  `userid` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `nom` varchar(100) NOT NULL,
+  `prenom` varchar(100) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `lang` varchar(10) NOT NULL DEFAULT 'fr',
+  `reg_date` datetime NOT NULL,
+  `active` int(1) NOT NULL
+);
